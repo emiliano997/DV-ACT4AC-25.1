d2c9cdc674eef9342ca16874a44d33121757e2f6
emilianoperez <emi.perez997@gmail.com>
2025-04-25 17:23:36 -0300

    [ADD] Clase 5

---
+++ clase_5/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Hero } from "./components/Hero";
+
+function App() {
+  return (
+    <>
+      {/* <Hero></Hero> */}
+      <Hero />
+    </>
+  );
+}
+
+export default App;
---
+++ clase_5/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import "./Hero.css";
+
+// Funcitón Flecha
+const HeroArrow = () => {};
+
+// Función Tradicional
+export function Hero() {
+  return (
+    <header className="container">
+      <h1> Titulo </h1>
+    </header>
+  );
+}
